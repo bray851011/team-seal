0a2124d8b36048381520bd9941c5add13742978e
Po Jui Juan <bray851011@gmail.com>
2024-01-28 23:16:16 -0500

    Update Dropdown Component

--- src/components/dropdown/dropdown.component.jsx
+++ src/components/dropdown/dropdown.component.jsx
@@ -9,6 +9,7 @@ const Dropdown = () => {
                 <option value="option1">Option 1</option>
                 <option value="option2">Option 2</option>
                 <option value="option3">Option 3</option>
+                <option value="option3">Option 3</option>
             </select>
         </div>
     )
