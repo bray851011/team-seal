0f4ec8e219c0d9f65d9ec2e66ea39e77b1c6a604
Po Jui Juan <bray851011@gmail.com>
2024-02-02 22:06:28 -0500

    Update Nav Bar

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,14 +1,35 @@
+import { useState, useEffect } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
+import seal from '../../assets/seal.png'
+
 
 import './navigation.styles.scss'
 
 const Navigation = () => {
+    const linkTexts = ['海豹泳隊。', '泳者無懼。']; // Define your link texts here
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [fadeClass, setFadeClass] = useState('fade-in');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setFadeClass('fade-out');
+    
+          setTimeout(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % linkTexts.length);
+            setFadeClass('fade-in');
+          }, 1000); // Matches the fade-out animation time
+        }, 4000); // Change text every 2 seconds
+    
+        return () => clearInterval(interval); // Cleanup the interval on component unmount
+      }, []);
     return (
         <Fragment>
             <div className="navigation">
                 <Link className="logo-container" to='/'>
-                    海豹泳隊。泳者無懼。
+                    <img src={seal} />
+                    <p className={fadeClass}>{linkTexts[currentIndex]}</p>
+
                 </Link>
                 <div className="nav-links-container">
                     <Link className="nav-link" to='/sign-in'>
