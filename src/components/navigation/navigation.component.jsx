cff60c2606b9a404f53bbd6195cdfd4d91a63820
Bray <bray851011@gmail.com>
2024-07-08 23:04:01 -0400

    Update NavBar

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -38,38 +38,37 @@ const Navigation = () => {
     return (
         <Fragment>
             <div className="navigation">
-                
-                <div className="temp">
-                    <Link className="logo-container" to='/'>
-                        <img src={seal} alt=""/>
-                        <p className={fadeClass}>{linkTexts[currentIndex]}</p>
-                    </Link>
+                <div className="container">
+                    <div className="logo">
+                        <Link className="logo-container" to='/'>
+                            <img src={seal} alt=""/>
+                            <p className={fadeClass}>{linkTexts[currentIndex]}</p>
+                        </Link>
 
-                    <button className="hamburger" onClick={menuClick}>
-                        <i className="menuIcon material-icons">menu</i>
-                    </button>
+                        <button className="hamburger" onClick={menuClick}>
+                            <i className="menuIcon material-icons">menu</i>
+                        </button>
+                    </div>
+                    
+                    <div className={`nav-links-container ${isNavExpanded ? 'hide' : 'show'}`}>
+                        <Link className="nav-link" to='/sign-in'onClick={menuClick}>
+                            泳隊成員
+                        </Link>
+                        <Link className="nav-link" to='/inquiry' onClick={menuClick}>
+                            成績查詢
+                        </Link>
+                        <Link className="nav-link" to='/sign-in' onClick={menuClick}>
+                            比賽成果
+                        </Link>
+        
+                        <Link className="nav-link" to='/sign-in'>
+                            贊助泳隊
+                        </Link>
+                        <Link className="nav-link" to='/login'>
+                            登入系統
+                        </Link> 
+                    </div>
                 </div>
-                
-                
-                <div className={`nav-links-container ${isNavExpanded ? 'hide' : 'show'}`}>
-                    <Link className="nav-link" to='/sign-in'onClick={menuClick}>
-                        泳隊成員
-                    </Link>
-                    <Link className="nav-link" to='/inquiry' onClick={menuClick}>
-                        成績查詢
-                    </Link>
-                    <Link className="nav-link" to='/sign-in' onClick={menuClick}>
-                        比賽成果
-                    </Link>
-    
-                    <Link className="nav-link" to='/sign-in'>
-                        贊助泳隊
-                    </Link>
-                    <Link className="nav-link" to='/login'>
-                        登入系統
-                     </Link> 
-                </div>
-            
             </div>
             <Outlet />
         </Fragment>
